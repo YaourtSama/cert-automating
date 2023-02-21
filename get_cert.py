44f7fa1db1be0a8911ad72b0bcbf320c95397221
FIANT Evan <evan.fiant@manche.fr>
2023-02-21 09:01:32 +0100

    added printing of useful infos

--- get_cert.py
+++ get_cert.py
@@ -3,13 +3,16 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 
 addr = 'www.manche.fr'
+
+#Récupération du certificat au format PEM
 pem_data = ssl.get_server_certificate((addr, 443))
 
 #Convertir le certificat du format PEM en object Certificate
 cert = x509.load_pem_x509_certificate(str.encode(pem_data), default_backend())
 
-#Affichage des informations du pays du certificat https://cryptography.io/en/latest/x509/reference/
-print(cert.subject.get_attributes_for_oid(x509.NameOID.COUNTRY_NAME)[0].value) 
-
-#print(cert.not_valid_before)
-print(cert.subject)
+#Affichage des informations du certificat https://cryptography.io/en/latest/x509/reference/
+print(cert.subject.get_attributes_for_oid(x509.NameOID.COMMON_NAME)[0].value) 
+print(cert.not_valid_after)
+print(cert.serial_number)
+print(cert.issuer.get_attributes_for_oid(x509.NameOID.COMMON_NAME)[0].value)
+#print(cert.subject)
